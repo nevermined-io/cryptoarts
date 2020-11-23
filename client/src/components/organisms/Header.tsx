3f102c56c420695e8831791bf5d922badb3a1efa
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-11-23 19:36:16 +0100

    Style improvements

--- client/src/components/organisms/Header.tsx
+++ client/src/components/organisms/Header.tsx
@@ -24,7 +24,17 @@ export default class Header extends PureComponent {
             <header className={styles.header}>
                 <div className={styles.headerContent}>
                     <NavLink to="/" className={styles.headerLogo}>
-                        <Logo className={styles.headerLogoImage} />
+                        <svg className={styles.headerLogoImage} width="550" height="191" viewBox="0 0 550 191" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M184.343 203.548L0 37.131V-131L184.315 35.391V-131L368.657 35.417L549.586 -131V37.131L368.657 203.548L184.343 37.157V203.548Z" fill="url(#paint0_linear)"/>
+                            <defs>
+                                <linearGradient id="paint0_linear" x1="0" y1="-131" x2="560.817" y2="-110.973" gradientUnits="userSpaceOnUse">
+                                    <stop stop-color="#7834F9"/>
+                                    <stop offset="1" stop-color="#67CFF9"/>
+                                </linearGradient>
+                            </defs>
+                        </svg>
+
+
                         <h1 className={styles.headerTitle}>{meta.title}</h1>
                     </NavLink>
 
