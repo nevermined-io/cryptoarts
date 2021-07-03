b41ebaa6dba68cf86954ec50c8c4fba94462c7d7
obasilakis <vasileios.georgopoulos@gmail.com>
2021-07-03 10:34:05 +0300

    Turn AccountStatus to functional component

--- client/src/components/molecules/AccountStatus.tsx
+++ client/src/components/molecules/AccountStatus.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React, { useState } from 'react'
 import { toDataUrl } from 'ethereum-blockies'
 import cx from 'classnames'
 
@@ -8,72 +8,56 @@ import styles from './AccountStatus.module.scss'
 import CircleButton from '../atoms/CircleButton'
 import { AccountConnectedIcon, AccountNotConnectedIcon, ArrowRightIcon, DisconnectIcon } from '../icons'
 
-interface AccountStatusState {
-    isPopoverOpen: boolean
+export const AccountStatus = () => {
+    const [isPopoverOpen, setIsPopoverOpen] = useState(false)
+
+    return (
+        <User.Consumer>
+            {({account, loginMetamask, isLogged}) => (
+                <div className={styles.wrapper}>
+                    {isLogged
+                        ? (
+                            <div onMouseOver={() => setIsPopoverOpen(true)} onMouseOut={() => setIsPopoverOpen(false)}>
+                                <CircleButton className={styles.button} secondary><AccountConnectedIcon /></CircleButton>
+                            </div>
+                        )
+                        : <CircleButton onClick={loginMetamask}><AccountNotConnectedIcon /></CircleButton>}
+
+                    <Market.Consumer>
+                        {market => !isLogged || (
+                            <div className={cx(styles.popup, isPopoverOpen ? styles.popupOpen : undefined)}>
+                                {market.networkMatch || true // <-- TODO: Remove the true
+                                    ? (
+                                        <>
+                                            <div className={cx(styles.row, styles.account)}>
+                                                <img
+                                                    className={styles.accountImage}
+                                                    src={account && toDataUrl(account)}
+                                                    alt="Blockies"
+                                                />
+                                                <div className={styles.accountAddress}>
+                                                  {account}
+                                                </div>
+                                            </div>
+                                            <div className={styles.row}>
+                                                <span>Go to profile</span>
+                                                <ArrowRightIcon />
+                                            </div>
+                                            <div className={styles.row}>
+                                                <span>Disconnect</span>
+                                                <DisconnectIcon />
+                                            </div>
+                                        </>
+                                    ) : (
+                                        <div></div>
+                                    )}
+                            </div>
+                        )}
+                    </Market.Consumer>
+                </div>
+            )}
+        </User.Consumer>
+    )
 }
 
-export default class AccountStatus extends PureComponent<
-    {},
-    AccountStatusState
-> {
-    public state = {
-        isPopoverOpen: false
-    }
-
-    private togglePopover() {
-        this.setState(prevState => ({
-            isPopoverOpen: !prevState.isPopoverOpen
-        }))
-    }
-
-    render() {
-        const {isPopoverOpen} = this.state
-        return (
-            <User.Consumer>
-                {({account, loginMetamask, isLogged}) => (
-                    <div className={styles.wrapper}>
-                        {isLogged
-                            ? (
-                                <div onMouseOver={() => this.togglePopover()} onMouseOut={() => this.togglePopover()}>
-                                    <CircleButton className={styles.button} secondary><AccountConnectedIcon /></CircleButton>
-                                </div>
-                            )
-                            : <CircleButton onClick={loginMetamask}><AccountNotConnectedIcon /></CircleButton>}
-
-                        <Market.Consumer>
-                            {market => !isLogged || (
-                                <div className={cx(styles.popup, isPopoverOpen ? styles.popupOpen : undefined)}>
-                                    {market.networkMatch || true // <-- TODO: Remove the true
-                                        ? (
-                                            <>
-                                                <div className={cx(styles.row, styles.account)}>
-                                                    <img
-                                                        className={styles.accountImage}
-                                                        src={account && toDataUrl(account)}
-                                                        alt="Blockies"
-                                                    />
-                                                    <div className={styles.accountAddress}>
-                                                      {account}
-                                                    </div>
-                                                </div>
-                                                <div className={styles.row}>
-                                                    <span>Go to profile</span>
-                                                    <ArrowRightIcon />
-                                                </div>
-                                                <div className={styles.row}>
-                                                    <span>Disconnect</span>
-                                                    <DisconnectIcon />
-                                                </div>
-                                            </>
-                                        ) : (
-                                            <div></div>
-                                        )}
-                                </div>
-                            )}
-                        </Market.Consumer>
-                    </div>
-                )}
-            </User.Consumer>
-        )
-    }
-}
+export default AccountStatus
