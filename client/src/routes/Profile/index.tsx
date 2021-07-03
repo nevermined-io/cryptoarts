4ffe34b68f780713d144a22b4c61793e438095b4
obasilakis <vasileios.georgopoulos@gmail.com>
2021-07-03 19:16:50 +0300

    Add create profile form

--- client/src/routes/Profile/index.tsx
+++ client/src/routes/Profile/index.tsx
@@ -6,14 +6,15 @@ import styles from './index.module.scss'
 import FullHeightView, { ContentRow } from '../../components/atoms/FullHeightView'
 import { CloseIcon } from '../../components/icons'
 import Button from '../../components/atoms/Button'
-import Steps, { Step } from '../../components/atoms/Steps'
 import { useForm } from '../../hooks/UseForm'
 import { History } from 'history'
+import Input from '../../components/atoms/MaterialForms/Input'
+import Select from '../../components/atoms/MaterialForms/Select'
+import { InputAdornment } from '@material-ui/core'
 
 function Profile({ history }: { history: History }) {
     const login = () => console.log('login')
     const validate = () => console.log('validate')
-    const [step, setStep] = useState(1)
     const {
         handleChange,
         handleSubmit,
@@ -36,8 +37,8 @@ function Profile({ history }: { history: History }) {
                         </div>
                     </div>
                     <div className={styles.preview}>
-                        <h1>Your Profile name</h1>
-                        <h2>@yourusername</h2>
+                        <h1>{values.name || 'Your Profile name'}</h1>
+                        <h2> || @yourusername</h2>
                     </div>
                 </>
             )}
@@ -46,11 +47,63 @@ function Profile({ history }: { history: History }) {
                     <div className={styles.formContent}>
                         <h1>Create your profile</h1>
                     </div>
+                    <Input
+                        className={styles.formField}
+                        label="Name"
+                        placeholder="What you call yourself"
+                        value={values.name || ''}
+                        name="name"
+                        onChange={handleChange}
+                    />
+                    <Input
+                        className={styles.formField}
+                        label="Username"
+                        placeholder="What others call you"
+                        value={values.username || ''}
+                        name="username"
+                        onChange={handleChange}
+                        helperText="Enter a unique title. You can tell us more in the description below too."
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start">
+                                <div className={styles.adornment}>
+                                    @
+                                </div>
+                            </InputAdornment>
+                        }}
+                    />
+                    <Input
+                        className={styles.formField}
+                        label="Email"
+                        placeholder="Your email address"
+                        value={values.email || ''}
+                        name="email"
+                        onChange={handleChange}
+                        helperText="If you want to get email updates. Not shown on profile."
+                    />
+                    <Select
+                        name="role"
+                        defaultValue="Artist"
+                        style={{ color: values.role ? 'black' : '#9D9FA0', margin: '10px 0 48px' }}
+                        value={values.role}
+                        label="What do you do"
+                        onChange={handleChange}
+                    >
+                        <option value="Artist">Artist</option>
+                        <option value="Collector">Collector</option>
+                    </Select>
+                    <Input
+                        className={styles.formField}
+                        multiline
+                        label="Bio description"
+                        value={values.description || ''}
+                        name="description"
+                        onChange={handleChange}
+                        placeholder="e.g. Went to the Mars School of Arts and Sciences and created the Mona Lisa of the modern generation"
+                    />
                     <Button
                         fullWidth
-                        onClick={() => setStep(step >=2 ? 3 : step + 1)}
                         secondary
-                    >next</Button>
+                    >complete profile</Button>
                 </>
             )}
         />
